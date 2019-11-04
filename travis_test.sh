c48f8deea3c693c9f6ef4b04f7681cbe06f46777
PraxisService <48514420+PraxisService@users.noreply.github.com>
2019-11-04 16:47:04 +0100

    Create travis_test.sh

---
+++ travis_test.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/sh
+
+# Search for PHP syntax errors.
+find -L . -name '*.php' -print0 | xargs -0 -n 1 -P 4 php -l
+# Run PHPCS.
+# If the custom ruleset is named `.phpcs.xml`, `phpcs.xml`, `.phpcs.xml.dist` or `phpcs.xml.dist`
+# and, like the travis script, is in the root of the project, you can leave
+# out the `--standard=` part of the command.
+if [[ "$SNIFF" == "1" ]]; then
+    RESULT=$PHPCS_DIR/bin/phpcs . --standard=./custom-ruleset.xml;
+    if [ "$RESULT" == 2 ]; then
+        exit 0;
+    else
+        exit 1;
+    fi
+fi
